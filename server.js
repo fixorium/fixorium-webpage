0dbb01e2a75e7ce4392d645d0c6fe4897e610c75
fixorium <fixercoin.token@gmail.com>
2025-07-05 12:00:21 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const api = require('./api');
 
+app.use(express.json());
+
+app.get('/api/blockchain/data', api.getBlockchainData);
+app.get('/api/wallet/balance', api.getWalletBalance);
+app.post('/api/token/deploy', api.deployToken);
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
