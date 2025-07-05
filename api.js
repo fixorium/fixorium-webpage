c9284eee59f3e7c77f37eadc668a7788ab9617ab
fixorium <fixercoin.token@gmail.com>
2025-07-05 12:01:48 +0500

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,31 @@
+ const fixoriumBlockchainApi = new FixoriumBlockchainApi();
+const fixoriumWalletApi = new FixoriumWalletApi();
 
+exports.getBlockchainData = (req, res) => {
+    fixoriumBlockchainApi.getBlockchainData().then((data) => {
+        res.json(data);
+    }).catch((error) => {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to retrieve blockchain data' });
+    });
+};
+
+exports.getWalletBalance = (req, res) => {
+    fixoriumWalletApi.getBalance().then((balance) => {
+        res.json({ balance });
+    }).catch((error) => {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to retrieve wallet balance' });
+    });
+};
+
+exports.deployToken = (req, res) => {
+    const tokenName = req.body.tokenName;
+    const tokenSymbol = req.body.tokenSymbol;
+    fixoriumWalletApi.deployToken(tokenName, tokenSymbol).then((tokenAddress) => {
+        res.json({ tokenAddress });
+    }).catch((error) => {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to deploy token' });
+    });
+};
